da3e71253e54a8d41512815ee349c1fc4127919a
wzg <wzg7014@163.com>
2025-06-07 00:03:03 +0800

    2-2 init

---
+++ src/test/java/com/wzg/wiki/WzgwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzg.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WzgwikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
