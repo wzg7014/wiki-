32001d765e1cd54e13abeda35e2d7d6c63035128
wzg <wzg7014@163.com>
2025-06-09 18:24:48 +0800

    3-6 增加通用返回类CommonResp，以后所有后端接口的返回值全部都是CommonResp类型

--- src/main/java/com/wzg/wiki/controller/EbookController.java
+++ src/main/java/com/wzg/wiki/controller/EbookController.java
@@ -1,5 +1,6 @@
 package com.wzg.wiki.controller;
 
+import Resp.CommonResp;
 import com.wzg.wiki.domain.Ebook;
 import com.wzg.wiki.service.EbookService;
 import jakarta.annotation.Resource;
@@ -17,7 +18,10 @@ public class EbookController {
     private EbookService service;
 
     @GetMapping("/list")
-    public List<Ebook> list(){
-        return service.list();
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = service.list();
+        resp.setContent(list);
+        return resp;
     }
 }
