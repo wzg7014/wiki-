b033cbc785f15df8c678c189144901b73cd7de2f
wzg <wzg7014@163.com>
2025-06-09 15:57:06 +0800

    ！ 注解开发改为xml开发

--- src/main/java/com/wzg/wiki/mapper/TestMapper.java
+++ src/main/java/com/wzg/wiki/mapper/TestMapper.java
@@ -1,15 +1,11 @@
 package com.wzg.wiki.mapper;
 
 import com.wzg.wiki.domain.Test;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
-@Mapper
 public interface TestMapper {
 
-    @Select("select * from test")
-    public List<Test> list();
+    List<Test> list();
 
 }
