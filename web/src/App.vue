831e830bc2ea129f1be07d22b8a45c64acaa1976
wzg <wzg7014@163.com>
2025-06-10 22:06:39 +0800

    4-6 制作the-header组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,24 +1,6 @@
 <template>
   <a-layout id="components-layout-demo-top-side-2">
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          theme="dark"
-          mode="horizontal"
-          v-model:selectedKeys="selectedkeys1"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">
-          nav 1
-        </a-menu-item>
-        <a-menu-item key="2">
-          nav 2
-        </a-menu-item>
-        <a-menu-item key="3">
-          nav 3
-        </a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header></the-header>
       <router-view/>
     <a-layout-footer style="text-align: center">
       彰广电子书
@@ -26,14 +8,23 @@
   </a-layout>
 </template>
 
-<style>
-#components-layout-demo-top-side-2 .logo {
-  width: 120px;
-  height: 31px;
-  background: rgba(255, 255, 255, 0.2);
-  margin: 16px 28px 16px 0;
-  float: left;
-}
-</style>
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeader from "@/components/the-header.vue";
+export default defineComponent({
+  name: 'app',
+  components:{
+    TheHeader,
+  }
+});
 </script>
+
+<style>
+  #components-layout-demo-top-side-2 .logo {
+    width: 120px;
+    height: 31px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px 28px 16px 0;
+    float: left;
+  }
+</style>
